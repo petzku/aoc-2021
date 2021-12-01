e533494c306c5701e4776d7a9473c38ecf98c3c2
petzku <petzku@zku.fi>
2021-12-01 13:15:37 +0200

    01-2: update python script (in like 2 mins)

--- sonar-01/sonar.py
+++ sonar-01/sonar.py
@@ -8,5 +8,11 @@ def count_increases(x):
     increased = [(p,n) for p,n in z if n > p]
     return len(increased)
 
+def sliding_window(vals):
+    return [sum(x) for x in zip(vals, vals[1:], vals[2:])]
+
 print("test:", count_increases(test))
 print("real:", count_increases(numbers))
+
+print("test:", count_increases(sliding_window(test)))
+print("real:", count_increases(sliding_window(numbers)))
