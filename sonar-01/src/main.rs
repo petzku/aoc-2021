4f555fa47ef60657bd72015d6e9a256327b514c8
petzku <petzku@zku.fi>
2021-12-01 13:32:27 +0200

    01-2: rust implementation

--- sonar-01/src/main.rs
+++ sonar-01/src/main.rs
@@ -3,14 +3,20 @@ use std::fs;
 fn main() {
     let f = "sonar.input.txt";
     let depths = read_depths_from_file(f);
-    let incs = count_depth_increases(depths);
+
+    // part 1
+    let incs = count_depth_increases(&depths);
     println!("increased {} times", incs);
+
+    // part 2
+    let incs = count_depth_increases(&sliding_windows(&depths));
+    println!("window increased {} times", incs)
 }
 
-fn count_depth_increases(depths: Vec<u64>) -> u64 {
+fn count_depth_increases(depths: &Vec<u64>) -> u64 {
     let mut n = 0;
     let mut prev = &depths[0];
-    for x in &depths {
+    for x in depths {
         if x > prev {
             n += 1;
         }
@@ -26,6 +32,14 @@ fn read_depths_from_file(filename: &str) -> Vec<u64> {
     contents.split_whitespace().map(|s| s.parse().expect("parse error")).collect()
 }
 
+fn sliding_windows(v: &Vec<u64>) -> Vec<u64> {
+    v.iter()
+        .zip(&v[1..])
+        .zip(&v[2..])
+        .map( |((a,b), c)| a+b+c)
+        .collect()
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -34,6 +48,19 @@ mod tests {
     #[test]
     fn basic_depths() {
         let depths = vec![ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ];
-        assert_eq!(count_depth_increases(depths), 7);
+        assert_eq!(count_depth_increases(&depths), 7);
+    }
+
+    #[test]
+    fn simple_sliding() {
+        let depths = vec![ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ];
+        let sums = sliding_windows(&depths);
+        assert_eq!(sums, vec![ 607, 618, 618, 617, 647, 716, 769, 792 ]);
+    }
+
+    #[test]
+    fn sliding_compare() {
+        let depths = vec![ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ];
+        assert_eq!(count_depth_increases(&sliding_windows(&depths)), 5);
     }
 }
